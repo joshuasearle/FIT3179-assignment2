41cbc3b58e1741f4804816e7fb2c93b290856dfe
Josh Searle <joshryansearle@gmail.com>
2021-10-15 10:57:25 +1100

    Add highest lowest graphs

---
+++ wrangling/top_ten.js
@@ -0,0 +1,62 @@
+const fs = require('fs');
+
+let data = String(fs.readFileSync('../data/clean.csv')).split('\n');
+
+data = data.map(row => {
+  return row.trim().split(',')
+})
+
+data.splice(0, 1)
+
+
+const years = new Set();
+const countries = new Set();
+
+data.forEach(row => {
+  countries.add(row[0]);
+  years.add(row[1])
+})
+
+const continentdata = String(fs.readFileSync('../data/continents.txt')).split('\n');
+const continents = {}
+const set = new Set()
+
+continentdata.forEach(row => {
+  [country, continent] = row.trim().split(',')
+  continents[country] = continent
+  set.add(continent)
+})
+
+data.forEach(row => {
+  row.push(continents[row[0]])
+})
+
+data.unshift(['country', 'year', 'type', 'value', 'continent'])
+
+let ranks = [['country', 'actual', 'continent', 'year', 'type', 'value', 'rank', 'direction']]
+
+for (c of ['Africa', 'Asia', 'Europe', 'South America', 'North America', 'Oceania', 'All']) {
+  for (y of years) {
+    for (t of ['both_suicide', 'male_suicide', 'female_suicide']) {
+      let filtered = data.filter(row => {
+        return row[1] == y && (row[4] == c || c == 'All') && row[2] == t
+      })
+
+      filtered.sort((a, b) => b[3] - a[3])
+      for (let i = 0; i < 10; i++) {
+        ranks.push([filtered[i][0], filtered[i][4], c, y, t, filtered[i][3], i + 1, 'highest'])
+      }
+
+      filtered.sort((a, b) => a[3] - b[3])
+      for (let i = 0; i < 10; i++) {
+        ranks.push([filtered[i][0], filtered[i][4], c, y, t, filtered[i][3], i + 1, 'lowest'])
+      }
+    }
+  }
+}
+
+
+console.log(ranks.slice(0, 10));
+ranks = ranks.map(row => row.join(',')).join('\n')
+
+fs.writeFileSync('../data/top_ten.csv', ranks)
